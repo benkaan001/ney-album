a64e715b9a8932b1ff14fb99a4f536cd21e3e5a5
ben kaan <benkaan001@gmail.com>
2022-04-09 19:39:48 -0500

    set up canvas

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('go');
+/*
+fire the load event whent the page has been loaded with static files
+
+getElementById is slightly faster than querySelector
+*/
+window.addEventListener('load', function () {
+  const canvas = this.document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 600;
+});
