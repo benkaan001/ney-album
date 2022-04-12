24f0042408075715079be8256cfcf79de45dd866
ben kaan <benkaan001@gmail.com>
2022-04-12 16:34:33 -0500

    add pseudo element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from 'react';
+
 import './App.scss';
 import Kitty from './assets/kitty.webp';
 
 function App() {
+  let container = useRef(null);
+  let image = useRef(null);
+
+  useEffect(() => {
+    console.log(container, image);
+  });
   return (
     <section className='main'>
-      <div className='container'>
+      <div ref={(element) => (container = element)} className='container'>
         <div className='img-container'>
-          <img src={Kitty} alt='cat' />
+          <>
+            <img ref={(element) => (image = element)} src={Kitty} alt='cat' />
+          </>
         </div>
       </div>
     </section>
